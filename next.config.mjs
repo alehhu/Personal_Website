f5cca17ccb063cd1f09928eb13273d9ff7b9ae20
alehhu <159355663+alehhu@users.noreply.github.com>
2024-05-30 18:47:15 +0200

    files for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/alehhu.github.io",
+  assetPrefix: '/alehhu.github.io/',
+  /**reactStrictMode: true,**/
   /**output: 'export',
   images: { unoptimized: true },**/
 };
