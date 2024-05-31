2032aecd26ec062e9521016e3c4eebec34fc6cf5
Alessandro Hu <159355663+alehhu@users.noreply.github.com>
2024-05-31 17:02:28 +0200

    Update next.config.mjs
    modified base path

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "/alehhu.github.io",
-  assetPrefix: '/alehhu.github.io/',
+  basePath: "/alehhu.github.io/Personal_Website/",
+  assetPrefix: '/alehhu.github.io/Personal_Website/',
   /**reactStrictMode: true,**/
   /**output: 'export',
   images: { unoptimized: true },**/
