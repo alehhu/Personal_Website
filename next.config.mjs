440c9a86edfd22b62d5def6f4a6596083a3c6863
Alessandro Hu <159355663+alehhu@users.noreply.github.com>
2024-05-31 17:05:47 +0200

    Update next.config.mjs
    Tried different assetPrefix path

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: "/alehhu.github.io/Personal_Website/",
-  assetPrefix: '/alehhu.github.io/Personal_Website/',
+  assetPrefix: '/alehhu.github.io/Personal_Website/out/_next/',
   /**reactStrictMode: true,**/
   /**output: 'export',
   images: { unoptimized: true },**/
