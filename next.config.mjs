a69fe17f6264c88d9912834a352b8d695144a292
alehhu <159355663+alehhu@users.noreply.github.com>
2024-06-02 17:48:44 +0200

    back to normal configuration for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/alehhu.github.io/Personal_Website",
-    assetPrefix: '/alehhu.github.io/Personal_Website',
-    output: 'export',
 };
 
 export default nextConfig;
